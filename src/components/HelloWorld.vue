c1b15e3ee072c7409ea38cfac880a319e15b06dc
rjimaw7 <rjimaw7777@gmail.com>
2024-02-28 13:34:53 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+    <div class="flex justify-center items-center flex-col gap-4">
+        <h1 class="text-2xl">{{ msg }}</h1>
+        <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+    </div>
+</template>
